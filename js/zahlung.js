abb45097f93cdebc11929721b7234d08578e0f85
manibauriedl <manibauriedl@gmail.com>
2026-08-29 17:14:14 +0200

    Auslieferung pausiert - Kauf und Download abgeschaltet, alte unsignierte Setups entfernt

--- js/zahlung.js
+++ js/zahlung.js
@@ -2,11 +2,11 @@
 window.ACRISUM_ZAHLUNG = {
   anbieter: "stripe",
   checkout_dynamisch: false,
-  checkout_url: "https://buy.stripe.com/28E00ifvdctw6zb3z5fIs09",
+  checkout_url: "",
   preis_api: "",
   danke_url: "https://acrisum.com/danke.html",
   danke_path: "danke.html",
-  download_url: "download/Acrisum-Launcher-Setup-1.2.8.exe",
+  download_url: "",
   button_bereit: "Jetzt kaufen",
   button_warten: "Bald kaufen",
   paypal_pflicht: true,
